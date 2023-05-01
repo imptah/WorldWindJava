f2c10405cbc100e0dbea055eeb1fc541f9f28f0f
imptah <serhii.halchynskyi@gmail.com>
2023-05-01 18:30:50 +0300

    - commented test OGLUtilTest.java

--- test/gov/nasa/worldwind/util/OGLUtilTest.java
+++ test/gov/nasa/worldwind/util/OGLUtilTest.java
@@ -2,29 +2,30 @@
  * Copyright 2006-2009, 2017, 2020 United States Government, as represented by the
  * Administrator of the National Aeronautics and Space Administration.
  * All rights reserved.
- * 
+ *
  * The NASA World Wind Java (WWJ) platform is licensed under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed
  * under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
- * 
+ *
  * NASA World Wind Java (WWJ) also contains the following 3rd party Open Source
  * software:
- * 
+ *
  *     Jackson Parser – Licensed under Apache 2.0
  *     GDAL – Licensed under MIT
  *     JOGL – Licensed under  Berkeley Software Distribution (BSD)
  *     Gluegen – Licensed under Berkeley Software Distribution (BSD)
- * 
+ *
  * A complete listing of 3rd Party software notices and licenses included in
  * NASA World Wind Java (WWJ)  can be found in the WorldWindJava-v2.2 3rd-party
  * notices and licenses PDF found in code directory.
- */
+ *//*
+
 
 package gov.nasa.worldwind.util;
 
@@ -163,7 +164,9 @@ public class OGLUtilTest
         assertEquals(td.getHeight(), 1024);
     }
 
-    /** Test that the newTextureData supports indexed color PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-369. */
+    */
+/** Test that the newTextureData supports indexed color PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-369. *//*
+
     @Test
     public void testIndexedColorPng() throws IOException
     {
@@ -173,7 +176,9 @@ public class OGLUtilTest
         assertEquals(td.getPixelFormat(), GL.GL_RGBA);
     }
 
-    /** Test that the newTextureData supports interlaced PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-365. */
+    */
+/** Test that the newTextureData supports interlaced PNG images. See http://issues.worldwind.arc.nasa.gov/jira/browse/WWJ-365. *//*
+
     @Test
     public void testInterlacedPng() throws IOException
     {
@@ -185,3 +190,4 @@ public class OGLUtilTest
         assertEquals(td.getHeight(), 32);
     }
 }
+*/
